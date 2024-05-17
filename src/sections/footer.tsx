e81a850f48790ecc273b8f03cc25eaef347c6061
Bia Miranda <Bia@Macs-MacBook-Pro.local>
2024-05-16 23:05:22 -0300

    fix: fixing footer translation

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -1,11 +1,14 @@
+import { useTranslation } from "react-i18next";
 import BiaLogo from "../assets/logo-bia.png";
 
 export function Footer() {
+    const { t } = useTranslation();
+
     return (
         <footer className="footer">
         <div className="container p-12 flex justify-between">
         <img src={BiaLogo} alt="Bia Logo" width={40} height={40}/>
-            <p className="text-slate-600">© 2024 Ana Beatriz Miranda Oliveira, Todos os direitos reservados.</p>
+            <p className="text-slate-600">{t('app.footer')}</p>
         </div>
         </footer>
     )
